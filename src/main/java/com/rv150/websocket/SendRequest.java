52f2aab188d212024a8851117171d2ad61cf4b4b
Ivan Rudnev <rudnev.vanya@gmail.com>
2017-05-15 16:21:11 +0300

    Fix default constructor

--- src/main/java/com/rv150/websocket/SendRequest.java
+++ src/main/java/com/rv150/websocket/SendRequest.java
@@ -4,12 +4,16 @@ package com.rv150.websocket;
  * Created by ivan on 15.05.17.
  */
 public class SendRequest {
-    private final String fileName;
-    private final String receiverId;
+    private String fileName;
+    private String receiverId;
 
     public SendRequest(String receiverId, String fileName) {
         this.fileName = fileName;
         this.receiverId = receiverId;
+    }
+
+    public SendRequest() {
+
     }
 
     public String getFileName() {
